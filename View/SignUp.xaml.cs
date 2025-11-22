7167e7592dc997299fbf6772b5ad256349bd8428
Roman <Romangorbunov4890@gmail.com>
2025-11-22 19:51:40 +0300

    SignUp fields now signed

--- View/SignUp.xaml.cs
+++ View/SignUp.xaml.cs
@@ -19,6 +19,45 @@ public partial class SignUp : Page
         
         
     }
+    
+    private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        var tb = sender as TextBox;
+        if (tb.Text == "Name" || tb.Text == "Password" || tb.Text == "Repeat Password")
+        {
+            tb.Text = "";
+        }
+    }
+    
+    private void Password_LostFocus(object sender, RoutedEventArgs e)
+    {
+        var tb = sender as TextBox;
+        if (string.IsNullOrEmpty(tb.Text))
+        {
+            tb.Text = "Password";
+        }
+    }
+    private void Repeat_Password_LostFocus(object sender, RoutedEventArgs e)
+    {
+        var tb = sender as TextBox;
+        if (string.IsNullOrEmpty(tb.Text))
+        {
+            tb.Text = "Repeat Password";
+        }
+    }
+    
+    private void Name_LostFocus(object sender, RoutedEventArgs e)
+    {
+        var tb = sender as TextBox;
+        if (string.IsNullOrEmpty(tb.Text))
+        {
+            tb.Text = "Name";
+        }
+    }
+    
+    
+    
+    
     private void SignUpButton_Click(object sender, RoutedEventArgs e)
     {
         
